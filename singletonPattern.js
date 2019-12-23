c19ce38758df6b56cce11df1a4cf606e19f5d0aa
Sahana R <sahana@ensurity.in>
2019-12-23 18:59:32 +0530

    singleton modified

--- singletonPattern.js
+++ singletonPattern.js
@@ -1,8 +1,15 @@
+class Car {
+    constructor(brand) {
+      this.carname = brand;
+    }
+  }
+  
 var Singleton = (function () {
     var instance;
- 
+    
     function createInstance() {
-        var object = new Object("I am the instance");
+        // var object = new Object("I am the instance");
+        var object = new Car('ford');
         return object;
     }
  
@@ -21,7 +28,8 @@ function run() {
     var instance1 = Singleton.getInstance();
     var instance2 = Singleton.getInstance();
  
-    console.log("Same instance? " + (instance1 === instance2));  
+    console.log("Same instance? ", instance1,instance2  + (instance1 === instance2));  
 }
+// Object.freeze() -- The freezing an object prevents new properties from being added to it, existing properties from being removed, prevent changing the enumerability,
 
 run();
